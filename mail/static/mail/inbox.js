8e556b4e3695561deca5e91e0b7f3dae8f68bb46
AntoineSau <69819028+AntoineSau@users.noreply.github.com>
2022-05-05 16:59:05 +0200

    Retrieving adn displaying sent emails

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -15,6 +15,10 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  
+  // Test
+  document.querySelector('#test').style.display = 'block';
+  document.querySelector('#test').innerHTML = `Test prints here-> ${mailbox}`;
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -27,7 +31,23 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  
+  // Test
+  document.querySelector('#test').style.display = 'block';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Test Fetching data from (1.) inbox and (2.) the requested mailbox {mailbox}
+  // ERROR inbox and arhcived becasue of recipeints error from Django Admin
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    // check on Console what data is passed in
+    console.log(emails);
+    // Print in HTML
+    emails.forEach(element => document.querySelector('#test').innerHTML = `SENT TO ${element.recipients} ON ${element.timestamp}. SUBJECT: ${element.subject}`)
+  });
+
+
 }
