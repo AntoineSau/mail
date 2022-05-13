f0253eda27f957f4eae8876de423e04574b53652
AntoineSau <69819028+AntoineSau@users.noreply.github.com>
2022-05-13 15:55:50 +0200

    Saving new email sent by post, need to redirect to sent emails

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,8 +6,32 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  // Test Form getting data but not posting
+  document.querySelector('form').onsubmit = function () {
+
+    // Getting data from form
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: document.querySelector('#compose-recipients').value,
+        subject: document.querySelector('#compose-subject').value,
+        body: document.querySelector('#compose-body').value
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      // Print result
+      console.log(result);
+    });
+
+    // Stop form from submitting
+    return false;
+    
+  } 
+
   // By default, load the inbox
-  load_mailbox('inbox');
+  load_mailbox('inbox')
+
 });
 
 function compose_email() {
@@ -16,9 +40,8 @@ function compose_email() {
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
   
-  // Test
+  // Test Print
   document.querySelector('#test').style.display = 'block';
-  document.querySelector('#test').innerHTML = `Test prints here-> ${mailbox}`;
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -39,15 +62,13 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
   // Fetching data from the requested mailbox {mailbox}
-  // ERROR inbox and archived because of recipients error from Django Admin
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {
     // check on Console what data is passed in
     console.log(emails);
     // Looping over all emails from chosen mailbox and print each one: 
-    emails.forEach(element => document.querySelector('#emails-view').innerHTML += `<li>SENT TO ${element.recipients} ON ${element.timestamp}. SUBJECT: ${element.subject}</li>`)
+    emails.forEach(element => document.querySelector('#emails-view').innerHTML += `<div>SENT TO ${element.recipients} ON ${element.timestamp}. SUBJECT: ${element.subject}</div>`)
   });
-
 
 }
